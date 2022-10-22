7fd406b487944728e5360d07e4ccd712f25e40b2
Jaemyeong Jin <jaemyeong@me.com>
2022-10-23 05:31:56 +0900

    Closed #13

--- Sources/SDSMealAPI/Menu.swift
+++ Sources/SDSMealAPI/Menu.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftSoup
+import ErrorKit
 
 public struct Menu {
     
@@ -9,23 +10,27 @@ public struct Menu {
     
     public var kilocalories: Measurement<UnitEnergy>
     
-    public var image: URL?
+    public var imageURL: URL
     
-    public init(name: String, dishes: [Dish], kilocalories: Measurement<UnitEnergy>, image: URL?) {
+    public init(name: String, dishes: [Dish], kilocalories: Measurement<UnitEnergy>, imageURL: URL) {
         self.name = name
         self.dishes = dishes
         self.kilocalories = kilocalories
-        self.image = image
+        self.imageURL = imageURL
     }
 }
 
 extension Menu {
     
     public init(element: Element) throws {
+        guard let imageURL = try (element.nextElementSibling()?.select("img").first()?.attr("src")).flatMap(URL.init(string:)) else {
+            throw NilError()
+        }
+        
         self.name = try element.select(".menu-title").first()?.ownText() ?? ""
         self.dishes = try element.select(".menu-detail span").not(".menu-calorie").first()?.ownText().split(separator: ",").map { $0.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines )}.map(Dish.init(name:)) ?? []
         self.kilocalories = try Measurement(value: (element.select(".menu-detail .menu-calorie").first()?.ownText().trimmingCharacters(in: CharacterSet.decimalDigits.inverted)).flatMap(Double.init) ?? 0.0, unit: UnitEnergy.kilocalories)
-        self.image = try (element.nextElementSibling()?.select("img").first()?.attr("src")).flatMap(URL.init(string:))
+        self.imageURL = imageURL
     }
 }
 
